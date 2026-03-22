8ca3b147f42f255a97640e0f2d3e714f2b67b715
Sean <>
2026-03-22 17:07:08 -0400

    Added commands to disable dnsmasq

--- autorun.sh
+++ autorun.sh
@@ -13,6 +13,10 @@ cron_job1="@reboot XDG_RUNTIME_DIR=/run/user/$(id -u) ~/ugv_rpi/ugv-env/bin/pyth
 # Define the second cron job for starting Jupyter
 cron_job2="@reboot /bin/bash ~/ugv_rpi/start_jupyter.sh >> ~/jupyter_log.log 2>&1"
 
+# Define cron jobs for shutting down dnsmasq (for AccessPopup)
+cron_job3="@reboot sudo systemctl stop dnsmasq.service"
+cron_job4="@reboot sudo systemctl disable dnsmasq.service"
+
 # Check if the first cron job already exists in the user's crontab
 if crontab -l | grep -q "$cron_job1"; then
     echo "First cron job is already set, no changes made."
@@ -30,6 +34,25 @@ else
     (crontab -l 2>/dev/null; echo "$cron_job2") | crontab -
     echo "Second cron job added successfully."
 fi
+
+# check if 3rd, 4th cron jobs aleady exist in users crontab
+# if not then add them
+if crontab -l | grep -q "$cron_job3"; then
+    echo "Third cron job is already set, no changes made."
+else
+    # Add the second cron job for the user
+    (crontab -l 2>/dev/null; echo "$cron_job3") | crontab -
+    echo "Third cron job added successfully."
+fi
+
+if crontab -l | grep -q "$cron_job4"; then
+    echo "Fourth cron job is already set, no changes made."
+else
+    # Add the second cron job for the user
+    (crontab -l 2>/dev/null; echo "$cron_job4") | crontab -
+    echo "Fourth cron job added successfully."
+fi
+
 
 source $PWD/ugv-env/bin/activate && jupyter notebook --generate-config
 CONFIG_FILE=/home/$(logname)/.jupyter/jupyter_notebook_config.py
